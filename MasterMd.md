8cda695a2ba1e536dd94074c5ea76315b307dd67
danius03 <daniel.lehner030303@gmail.com>
2021-08-11 13:48:01 +0200

    Retrieving md files from the whole project, not only resource folder

--- MasterMd.md
+++ MasterMd.md
@@ -3,7 +3,15 @@ Ich bin die Markdown Datei Nr 3
 ***Ich bin fett und kursiv***
 
 ---
+---
+---
+Ich bin die Markdown Datei Nr 1
+
+**Ich bin fett**
 
 ---
-
 ---
+---
+Ich bin die Markdown Datei Nr 2
+
+*Ich bin kursiv*
--- MdSearcher/src/main/java/Main.java
+++ MdSearcher/src/main/java/Main.java
@@ -1,5 +1,12 @@
 
+import org.apache.commons.io.FileUtils;
+
 import java.io.*;
+import java.net.URISyntaxException;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args){
@@ -9,8 +16,21 @@ public class Main {
             String project = projects.readLine();
             while(project != null) {
                 Class c = Class.forName(project);
-                InputStream is = c.getClassLoader().getResources(project + ".md").nextElement().openStream();
-                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                String path = c.getProtectionDomain().getCodeSource().getLocation().toURI().getPath().replace("/target/classes/", "");
+                boolean recursive = true;
+
+                Collection files = FileUtils.listFiles(new File(path), null, recursive);
+                List<File> mdFiles = new ArrayList<>();
+                for (Iterator iterator = files.iterator(); iterator.hasNext();) {
+                    File file = (File) iterator.next();
+                    if (file.getName().endsWith(".md") || file.getName().endsWith(".markdown"))
+                        mdFiles.add(file);
+
+                }
+                mdFiles = mdFiles.stream().filter(distinctByKey( File::getName)).toList();
+                for (File f: mdFiles) {
+                    System.out.println(f.getAbsolutePath());
+                BufferedReader br = new BufferedReader(new FileReader(f));
                 String line = br.readLine();
                 while (line != null) {
                     bw.write(line);
@@ -22,19 +42,25 @@ public class Main {
                 if(project != null) {
                     bw.write("\n---");
                     bw.newLine();
-                    bw.write("\n---");
+                    bw.write("---");
                     bw.newLine();
-                    bw.write("\n---");
+                    bw.write("---");
                     bw.newLine();
                     bw.flush();
                 }
-
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             System.out.println("Klasse nicht gefunden! Prüfen Sie, ob diese Klasse existiert oder ob Sie den Namen in der OneClassOfProject.txt Datei richtig geschrieben haben!");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
         }
 
+    }
+    public static <T> Predicate<T> distinctByKey(Function<? super T, ?> keyExtractor) {
+        Set<Object> seen = ConcurrentHashMap.newKeySet();
+        return t -> seen.add(keyExtractor.apply(t));
     }
 }
